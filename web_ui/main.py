efa60eb11a1c93f33f8400e079e8b237a8922aac
JV Roig <jvroig@gmail.com>
2025-05-06 20:26:24 +0800

    stuff

--- web_ui/main.py
+++ web_ui/main.py
@@ -207,4 +207,4 @@ async def shutdown_event():
     logger.info("Shutting down OWLBEAR Web UI")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8069)
